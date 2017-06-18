012adda9239b200c535786681f26d161680e35b7
EhsanFox <EhsanFox@users.noreply.github.com>
2017-06-18 15:52:17 +0430

    Create smsg-2.php

---
+++ plugs/E/smsg-2.php
@@ -0,0 +1,9 @@
+<?PHP
+include("../../config/ranks/admin.php");
+include("../../config/token.php");
+$msg = file_get_contents("../../data/text");
+$ip = file_get_contents("../../data/ip");
+$id = file_get_contents("../../data/id");
+file_get_contents("http://api.telegram.org/bot".$token."/sendmessage?chat_id=".$id."&text=".$text);
+exec("php smsg.php");
+?>
